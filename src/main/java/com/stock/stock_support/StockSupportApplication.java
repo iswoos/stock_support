4dacda890a2345a4a46e2b0bb4a04625de6ba0ed
이승우 <swlee@joara.com>
2025-06-28 18:07:52 +0900

    Begin : First Setting

---
+++ src/main/java/com/stock/stock_support/StockSupportApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stock_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSupportApplication.class, args);
+	}
+
+}
